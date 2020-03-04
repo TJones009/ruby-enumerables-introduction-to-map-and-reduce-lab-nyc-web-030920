ed4cbf78179ea63ad96a44a2f284344e7c0178f4
Trevor Jones <tjones009@yahoo.com>
2020-03-04 16:58:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,8 +37,10 @@ def map_to_square(array)
   i = 0
   
   while i < array.length do
-    new_array << (array[i])
-  
+    new_array << (array[i] * array[i])
+    i += 1
+  end
+  new_array
 end
   
   
